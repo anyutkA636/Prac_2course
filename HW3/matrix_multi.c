61b3359d86c7fdf58b2914daa3a563ee1ca8cd12
anyutkA636 <71659382+anyutkA636@users.noreply.github.com>
2022-09-24 01:37:19 +0300

    Create matrix_multi.c

---
+++ HW3/matrix_multi.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int vvod_arr (int *a, int m, int n)
+{
+  int i, j;
+  for (i = 0; i < m; i++)
+    for (j = 0; j < n; j++)
+      {
+	printf ("arr[%d][%d] = ", i, j);
+	scanf ("%d", &a[i * n + j]);
+      }
+}
+
+int print_arr (int *a, int m, int n)
+{
+  int i, j;
+  for (i = 0; i < m; i++)
+    {
+      for (j = 0; j < n; j++)
+	  printf ("%3d", a[i * n + j]);
+      printf ("\n");
+    }
+}
+
+int multi (int *A, int *B, int *C, int m, int n, int k)
+{
+  int i, j, h;
+  for(i = 0; i < m; i++)
+    for(j = 0; j < k; j++)
+    {
+        C[i * k + j] = 0;
+        for(h = 0; h < n; h++)
+            C[i * k + j] += A[i * n + h] * B[h * k + j];
+    }
+}
+
+int main(int argc, int *argv[])
+//int main (void)
+{
+  int n, m, k;
+if (argc < 4) 
+   printf("Incorrect_param\n");
+else {
+  m = *argv[1];
+  n = *argv[2];
+  k = *argv[3];
+  //scanf ("%d", &m);
+  //scanf ("%d", &n);
+  //scanf ("%d", &k);
+
+  int A_arr[m][n], B_arr[n][k], C_arr[m][k];
+  printf ("Enter array A:\n");
+  vvod_arr ((int *) A_arr, m, n);
+  
+  printf ("Enter array B:\n");
+  vvod_arr ((int *) B_arr, n, k);
+  
+  multi ((int *) A_arr, (int*)B_arr, (int*)C_arr, m, n ,k);
+  
+  //printf("Array A:\n");
+  //print_arr((int*)A_arr, m, n);
+  //printf("Array B:\n");
+  //print_arr((int*)B_arr, n, k);
+  
+  printf("Array C:\n");
+  print_arr((int*)C_arr, m, k);
+ }
+}
